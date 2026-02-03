815fa9e3f158ebc93074b2b5eff1a4aca400dd3d
AlinLucianBrb <93444805+AlinLucianBrb@users.noreply.github.com>
2026-02-03 10:32:07 +0200

    Updated Rendering

--- Assets/Scripts/Rendering.cs
+++ Assets/Scripts/Rendering.cs
@@ -41,20 +41,42 @@ public class Rendering : MonoBehaviour
     ComputeBuffer textureBuffer;
 
     public ComputeShader computeShader;
-    RenderTexture renderTexture;
+
+    // Multi-pass RTs
+    RenderTexture baseRT;
+    RenderTexture smokeRT;
+    RenderTexture smokeTempRT;
+    RenderTexture smokeBlurRT;
+    RenderTexture waterRT;
+    RenderTexture finalRT;
 
     int width, height;
-    int kernel;
+
+    // Kernels
+    int kShade;
+    int kBlurH;
+    int kBlurV;
+    int kComposite;
+
+    // Thread group size (must match [numthreads(8,8,1)])
+    const int TX = 8;
+    const int TY = 8;
 
     void Start()
     {
         width = World.instance.chunkSize * World.instance.noChunks.x;
         height = World.instance.chunkSize * World.instance.noChunks.y;
 
-        kernel = computeShader.FindKernel("CSMain");
+        // Find kernels by name (your compute shader must have these)
+        kShade = computeShader.FindKernel("Shade");
+        kBlurH = computeShader.FindKernel("BlurH");
+        kBlurV = computeShader.FindKernel("BlurV");
+        kComposite = computeShader.FindKernel("Composite");
 
         textureNativeArray = new NativeArray<uint>(World.instance.blocksData.Length, Allocator.Persistent);
-        textureBuffer = new ComputeBuffer(textureNativeArray.Length, sizeof(uint) * 4);
+
+        // IMPORTANT: stride must be sizeof(uint), not sizeof(uint) * 4
+        textureBuffer = new ComputeBuffer(textureNativeArray.Length, sizeof(uint));
     }
 
     void Update()
@@ -71,38 +93,100 @@ public class Rendering : MonoBehaviour
         };
 
         JobHandle jobHandle = renderingUpdateJob.Schedule(World.instance.blocksData.Length, 128);
-        jobHandle.Complete();       
+        jobHandle.Complete();
+    }
+
+    void EnsureRT(ref RenderTexture rt, int w, int h, RenderTextureFormat format, FilterMode filter)
+    {
+        if (rt != null && rt.width == w && rt.height == h && rt.format == format)
+            return;
+
+        if (rt != null) rt.Release();
+
+        rt = new RenderTexture(w, h, 0, format);
+        rt.enableRandomWrite = true;
+        rt.filterMode = filter;
+        rt.wrapMode = TextureWrapMode.Clamp;
+        rt.Create();
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        if (renderTexture == null)
-        {
-            renderTexture = new RenderTexture(World.instance.chunkSize * World.instance.noChunks.x, World.instance.chunkSize * World.instance.noChunks.y, 1);
-            renderTexture.enableRandomWrite = true;
-            renderTexture.filterMode = FilterMode.Point;
-            renderTexture.Create();
-        }
-     
+        int w = width;
+        int h = height;
+
+        // Allocate RTs
+        // Base + final color
+        EnsureRT(ref baseRT, w, h, RenderTextureFormat.ARGB32, FilterMode.Point);
+        EnsureRT(ref finalRT, w, h, RenderTextureFormat.ARGB32, FilterMode.Point);
+
+        // Masks (single channel float)
+        EnsureRT(ref smokeRT, w, h, RenderTextureFormat.RFloat, FilterMode.Point);
+        EnsureRT(ref smokeTempRT, w, h, RenderTextureFormat.RFloat, FilterMode.Point);
+        EnsureRT(ref smokeBlurRT, w, h, RenderTextureFormat.RFloat, FilterMode.Point);
+        EnsureRT(ref waterRT, w, h, RenderTextureFormat.RFloat, FilterMode.Point);
+
+        // Upload ids
         textureBuffer.SetData(textureNativeArray);
 
-        computeShader.SetInt("Width", width);
-        computeShader.SetBuffer(kernel, "Ids", textureBuffer);
-        computeShader.SetTexture(kernel, "Result", renderTexture);
+        int groupsX = (w + TX - 1) / TX;
+        int groupsY = (h + TY - 1) / TY;
 
-        // Must dispatch in 2D
-        int tx = 8, ty = 8; // must match numthreads in shader
-        int groupsX = (width + tx - 1) / tx;
-        int groupsY = (height + ty - 1) / ty;
+        // Common uniforms
+        computeShader.SetInt("Width", w);
+        computeShader.SetInt("Height", h);
 
-        computeShader.Dispatch(kernel, groupsX, groupsY, 1);
+        // -------------------------
+        // Pass A: Shade -> baseRT + smokeRT + waterRT
+        // -------------------------
+        computeShader.SetBuffer(kShade, "Ids", textureBuffer);
+        computeShader.SetTexture(kShade, "BaseOut", baseRT);
+        computeShader.SetTexture(kShade, "SmokeOut", smokeRT);
+        computeShader.SetTexture(kShade, "WaterOut", waterRT);
 
-        Graphics.Blit(renderTexture, destination);
+        computeShader.Dispatch(kShade, groupsX, groupsY, 1);
+
+        // -------------------------
+        // Pass B: BlurH smokeRT -> smokeTempRT
+        // -------------------------
+        computeShader.SetTexture(kBlurH, "InTex", smokeRT);
+        computeShader.SetTexture(kBlurH, "OutTex", smokeTempRT);
+
+        computeShader.Dispatch(kBlurH, groupsX, groupsY, 1);
+
+        // -------------------------
+        // Pass C: BlurV smokeTempRT -> smokeBlurRT
+        // -------------------------
+        computeShader.SetTexture(kBlurV, "InTex", smokeTempRT);
+        computeShader.SetTexture(kBlurV, "OutTex", smokeBlurRT);
+
+        computeShader.Dispatch(kBlurV, groupsX, groupsY, 1);
+
+        // -------------------------
+        // Pass D: Composite -> finalRT
+        // -------------------------
+        computeShader.SetTexture(kComposite, "BaseIn", baseRT);
+        computeShader.SetTexture(kComposite, "SmokeBlur", smokeBlurRT);
+        computeShader.SetTexture(kComposite, "WaterMask", waterRT);
+        computeShader.SetTexture(kComposite, "Result", finalRT);
+
+        computeShader.Dispatch(kComposite, groupsX, groupsY, 1);
+
+        // Output
+        EnsureRT(ref finalRT, w, h, RenderTextureFormat.ARGB32, FilterMode.Bilinear);
+        Graphics.Blit(finalRT, destination);
     }
 
     private void OnDestroy()
     {
-        textureNativeArray.Dispose();
-        textureBuffer.Dispose();
+        if (textureNativeArray.IsCreated) textureNativeArray.Dispose();
+        if (textureBuffer != null) textureBuffer.Dispose();
+
+        if (baseRT != null) baseRT.Release();
+        if (smokeRT != null) smokeRT.Release();
+        if (smokeTempRT != null) smokeTempRT.Release();
+        if (smokeBlurRT != null) smokeBlurRT.Release();
+        if (waterRT != null) waterRT.Release();
+        if (finalRT != null) finalRT.Release();
     }
 }
